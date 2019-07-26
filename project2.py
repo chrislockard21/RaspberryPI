24f7b52ede991d5a3b78cff4c2b2c2a5d2a5d559
Chris Lockard <chris.lockard21@gmail.com>
2019-07-26 12:45:18 -0400

    Final

--- project2.py
+++ project2.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as gpio
 from time import sleep, time
-from gpiozero import DistanceSensor
 import threading as thread
+import datetime
 
 def RGB_setup(red, green, blue):
 	'''
@@ -9,6 +9,7 @@ def RGB_setup(red, green, blue):
 	'''
 	gpio.setup((red, green, blue), gpio.OUT)
 	return True
+
 
 def change_color(distance, red, green, blue):
 	'''
@@ -43,7 +44,7 @@ def change_color(distance, red, green, blue):
 	green.ChangeDutyCycle(green_dc)
 	
 	
-	return distance, (red_dc, blue_dc)
+	return distance, (red_dc, green_dc, blue_dc)
 
 
 def read_distance(trig, echo, red, green, blue):
@@ -53,7 +54,7 @@ def read_distance(trig, echo, red, green, blue):
 	print/change the hue of the led.
 	'''
 	while True:
-			sleep(.5)
+			sleep(.1)
 			gpio.output(trig, True)
 			sleep(0.00001)
 			gpio.output(trig, False)
@@ -70,19 +71,30 @@ def read_distance(trig, echo, red, green, blue):
 			distance = round(distance, 2)
 			
 			if mode == 'Monitor':
-				change_color(distance, RED, GREEN, BLUE)
-				print(distance)
+				colors = change_color(distance, RED, GREEN, BLUE)
+				print(
+					'{} cm'.format(distance),
+					'RED:{}'.format(colors[1][0]),
+					'GREEN:{}'.format(colors[1][1]),
+					'BLUE:{}'.format(colors[1][2])
+				)
 				
 			elif mode == 'Monitor&Record':
-				change_color(distance, RED, GREEN, BLUE)
-				print(distance)
-				# something here to write to the file
+				colors = change_color(distance, RED, GREEN, BLUE)
+				print(
+					'{} cm'.format(distance),
+					'RED:{}'.format(str(colors[1][0])),
+					'GREEN:{}'.format(colors[1][1]),
+					'BLUE:{}'.format(colors[1][2])
+				)
+				f.write('SONIC,{},{} cm\n'.format(datetime.datetime.now(), distance))
 			
 			elif mode == 'Record':
 				# something here to write to the file
 				red.ChangeDutyCycle(0)
 				blue.ChangeDutyCycle(0)
 				green.ChangeDutyCycle(0)
+				f.write('SONIC,{},{} cm\n'.format(datetime.datetime.now(), distance))
 	
 
 def led_controller(led):
@@ -111,11 +123,37 @@ def binary_conversion(pot1, pot2, pot3, pot4):
 	return int(binary, 2)
 	
 
+def buz_freq(position):
+	'''
+	Function that uses a trend line I made in excel to change the freq
+	of the buzzer.
+	'''
+	freq = 133.33*position
+	# Freq has to be higher than 0
+	if freq == 0:
+		freq = 1
+	return freq
+
 def pot_reader(pot1, pot2, pot3, pot4, buzzer):
 	'''
 	Target funciton to control the pot funcitonality based on the mode
 	'''
-	pass
+	buzzer.ChangeDutyCycle(50)
+	while True:
+		sleep(.5)
+		position = binary_conversion(pot1, pot2, pot3, pot4)
+		percentage = position/15
+		freq = buz_freq(position)
+		buzzer.ChangeFrequency(freq)
+		if mode == 'Monitor':
+			print('{:.0%}'.format(percentage), freq)
+		elif mode == 'Monitor&Record':
+			print('{:.0%}'.format(percentage), freq)
+			f.write('POT,{},{:.0%}\n'.format(datetime.datetime.now(), percentage))
+		elif mode == 'Record':
+			f.write('POT,{},{:.0%}\n'.format(datetime.datetime.now(), percentage))
+		
+		
 
 if __name__ == '__main__':
 	# Constants for pin values
@@ -127,8 +165,6 @@ if __name__ == '__main__':
 	button = 16
 	trig = 25
 	echo = 24
-
-	# These are actually in the wrong order, but I didnt want to change
 	pot1 = 7
 	pot2 = 8
 	pot3 = 9
@@ -173,10 +209,12 @@ if __name__ == '__main__':
 	# Instantiates and runs the pot thread
 	# Side note, the pot is on the 5v channel so it will already be
 	# collecting data at the start of the thread
-	pot_thread = thread.Thread(target=pot_reader)
-	
+	pot_thread = thread.Thread(target=pot_reader, args=(pot1, pot2, pot3, pot4, BUZZ,))
+	pot_thread.start()
 	
 	try:
+		f = open('/home/pi/Documents/{}.txt'.format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:%S')), 'w')
+		f.write('Sensor,time,value\n')
 		time_count = 0
 		while True:
 			#pot_reader(pot1, pot2, pot3, pot4)
@@ -190,9 +228,17 @@ if __name__ == '__main__':
 					if time_count >= 2 and time_count < 5:
 						mode = 'Monitor&Record'
 						time_count = 0
+						f.close()
+						f = open('/home/pi/Documents/{}.txt'.format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:%S')), 'w')
+						f.write('Sensor,time,value\n')
+						print('Recording data... {}'.format(f.name))
 					elif time_count >= 5:
 						mode = 'Record'
 						time_count = 0
+						f.close()
+						f = open('/home/pi/Documents/{}.txt'.format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:%S')), 'w')
+						f.write('Sensor,time,value\n')
+						print('Recording data... {}'.format(f.name))
 				elif mode == 'Monitor&Record':
 					if time_count >= 2 and time_count < 5:
 						mode = 'Monitor'
@@ -200,16 +246,24 @@ if __name__ == '__main__':
 					elif time_count >= 5:
 						mode = 'Record'
 						time_count = 0
+						f.close()
+						f = open('/home/pi/Documents/{}.txt'.format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:%S')), 'w')
+						f.write('Sensor,time,value\n')
+						print('Recording data... {}'.format(f.name))
 				elif mode == 'Record':
 					if time_count >= 2 and time_count < 5:
 						mode = 'Monitor&Record'
 						time_count = 0
+						f.close()
+						f = open('/home/pi/Documents/{}.txt'.format(datetime.datetime.now().strftime('%Y-%m-%d:%H:%M:%S')), 'w')
+						f.write('Sensor,time,value\n')
+						print('Recording data... {}'.format(f.name))
 					elif time_count >= 5:
 						mode = 'Monitor'
 						time_count = 0
-			
-			print(mode)
+				
 				
 			sleep(1)
 	except KeyboardInterrupt:
 		gpio.cleanup()
+		f.close()
